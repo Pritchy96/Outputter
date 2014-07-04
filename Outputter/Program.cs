7c898af769570e985cde1e8fdbe752b0bcd3c126
Tom Pritchard <tompritchard1996@gmail.com>
2014-07-05 00:47:43 +0100

    Initial Commit

---
+++ Outputter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outputter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location.ToString());
+            string[] log = Directory.GetFiles(path, ".", SearchOption.AllDirectories);
+            File.WriteAllLines(path + "\\Output.txt", log);
+        }
+    }
+}
